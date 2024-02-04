257229506ebd86c96fb6f606b6cb6e717d7d11da
freddneos <fredd@neosdev.com.br>
2024-02-04 15:21:42 +0000

    chore: initial commit

---
+++ BreedApp/DogBreedService/DogBreedService.swift
@@ -0,0 +1,8 @@
+//
+//  DogBreedService.swift
+//  BreedApp
+//
+//  Created by Fredd Bezerra on 04/02/2024.
+//
+
+import Foundation
---
+++ BreedApp/DogBreedService/DogBreedServiceProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  DogBreedServiceProtocol.swift
+//  BreedApp
+//
+//  Created by Fredd Bezerra on 04/02/2024.
+//
+
+import Foundation
