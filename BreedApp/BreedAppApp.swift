42fbaabb47c60d5b01624f288a16cb8afd99a5bf
freddneos <fredd@neosdev.com.br>
2024-02-04 15:02:15 +0000

    Initial Commit

---
+++ BreedApp/BreedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreedAppApp.swift
+//  BreedApp
+//
+//  Created by Fredd Bezerra on 04/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct BreedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
